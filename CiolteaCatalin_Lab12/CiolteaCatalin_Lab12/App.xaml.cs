cd8a65f06feded8c6bf00b8249cc91568f53e773
Catalin Danut Cioltea <Cioltea.Sa.Catalin@student.utcluj.ro>
2022-01-13 20:03:26 +0200

    Add project files.

---
+++ CiolteaCatalin_Lab12/CiolteaCatalin_Lab12/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CiolteaCatalin_Lab12.Services;
+using CiolteaCatalin_Lab12.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CiolteaCatalin_Lab12.Data;
+
+
+namespace CiolteaCatalin_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WebAPI/Controllers/ShowListsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowListsController : ControllerBase
+    {
+        private readonly WebAPIContext _context;
+
+        public ShowListsController(WebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ShowLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ShowList>>> GetShowList()
+        {
+            return await _context.ShowList.ToListAsync();
+        }
+
+        // GET: api/ShowLists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ShowList>> GetShowList(int id)
+        {
+            var showList = await _context.ShowList.FindAsync(id);
+
+            if (showList == null)
+            {
+                return NotFound();
+            }
+
+            return showList;
+        }
+
+        // PUT: api/ShowLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShowList(int id, ShowList showList)
+        {
+            if (id != showList.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(showList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShowListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ShowLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ShowList>> PostShowList(ShowList showList)
+        {
+            _context.ShowList.Add(showList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShowList", new { id = showList.ID }, showList);
+        }
+
+        // DELETE: api/ShowLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShowList(int id)
+        {
+            var showList = await _context.ShowList.FindAsync(id);
+            if (showList == null)
+            {
+                return NotFound();
+            }
+
+            _context.ShowList.Remove(showList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ShowListExists(int id)
+        {
+            return _context.ShowList.Any(e => e.ID == id);
+        }
+    }
+}
